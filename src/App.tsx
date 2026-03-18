7a1a95007fac40af79dad9a54b1f40395c99084d
adebayostephen <adebayodamilola2007@gmail.com>
2026-03-18 16:21:33 +0100

    forgave client

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import BlockedPage from "./components/BlockedPage";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import StatsBar from "./components/StatsBar";
+import About from "./sections/About";
+import NFTGallery from "./components/NFTGallery";
+import Roadmap from "./sections/Roadmap";
+import Pokedex from "./sections/Pokedex";
+import Contact from "./sections/Contact";
+import Footer from "./components/Footer";
+import Layout from "./components/Layout";
+import Loader from "./components/Loader";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+    const [isLoading, setIsLoading] = useState(true);
+
     return (
-        <BlockedPage />
+        <div className="bg-background min-h-screen">
+            <AnimatePresence mode="wait">
+                {isLoading ? (
+                    <Loader key="loader" onComplete={() => setIsLoading(false)} />
+                ) : (
+                    <Layout key="main">
+                        <Navbar />
+                        <Hero />
+                        <StatsBar />
+                        <About />
+                        <NFTGallery />
+                        <Roadmap />
+                        <Pokedex />
+                        <Contact />
+                        <Footer />
+                    </Layout>
+                )}
+            </AnimatePresence>
+        </div>
     );
 }
 
 export default App;
-
