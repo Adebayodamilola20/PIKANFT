3e003704ebc1fe5e6a5bc6d57e4dc2e4649913fb
Lawal Giyath <giyathlawal@gmail.com>
2026-03-28 10:30:47 +0100

    fixes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -28,28 +28,6 @@ const Hero = ({ onApplyClick }: HeroProps) => {
                     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] bg-accent-blue/10 rounded-full blur-[150px]" />
                 </div>
 
-                {/* Floating Particles */}
-                {[...Array(20)].map((_, i) => (
-                    <motion.div
-                        key={i}
-                        className="absolute w-2 h-2 bg-primary/30 rounded-full"
-                        initial={{ 
-                            x: Math.random() * window.innerWidth, 
-                            y: Math.random() * window.innerHeight,
-                            scale: Math.random() * 0.5 + 0.5
-                        }}
-                        animate={{
-                            y: [null, Math.random() * window.innerHeight],
-                            x: [null, Math.random() * window.innerWidth],
-                        }}
-                        transition={{
-                            duration: Math.random() * 10 + 10,
-                            repeat: Infinity,
-                            repeatType: "reverse",
-                        }}
-                    />
-                ))}
-
                 <div className="container mx-auto px-6 relative z-10 flex flex-col items-center text-center">
                     {/* Badge */}
                     <motion.div
